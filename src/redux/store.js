087ff953eaaf4129bd72a5fa289657cac380726f
Ashish <abhagat629@gmail.com>
2021-05-28 16:54:35 +0530

    added redux structure

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,18 @@
-// Store
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { personReducer } from "./persons/reducer"
+
+let composeEnhancers = compose;
+
+if (process.env.NODE_ENV !== "production") {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+}
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+// Creating Store
+const store = createStore(personReducer, enhancer);
+
+export default store;
