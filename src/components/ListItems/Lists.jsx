3f6cc6702f4156acb1570ca562493494cd2e38ee
Ashish <abhagat629@gmail.com>
2021-05-29 19:47:28 +0530

    Added loader, key scroll

--- src/components/ListItems/Lists.jsx
+++ src/components/ListItems/Lists.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./index.css";
 
-const Lists = ({data, i}) => {
-    const {name, birth_year, gender } = data
-
+const Lists = ({ data, i, active,setActive, handleIndex, handlePerson }) => {
+    const { name, birth_year, gender } = data;
 
     // Displaying Items on search list
     return (
-        <div className="search-list">
+        <div
+            style={active === i ? { background: "darkgoldenrod", borderRadius:"0", color:"black" } : null}
+            onKeyUp = {(e) => handleIndex(e)}
+            onMouseEnter = {() => setActive(i)}
+            onClick={() => handlePerson(name)}
+            className="search-list"
+        >
             <div>
                 <p>{name}</p>
                 <span>{birth_year}</span>
